e534fe01e2a138d8cc73b037bb346fc561e6a2f1
Christian Flewelling <cflew@tutanota.com>
2022-10-22 22:47:56 +0000

    setup bezier_curve plotting test
    not sure if this is dependent on any changes i forgot to stage

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,9 +1,13 @@
 #include "../sea_current.hpp"
 
+#include "../third-party/matplotlib-cpp/matplotlibcpp.h"
+
 #include <Eigen/Dense>
 
 using namespace Eigen;
 using namespace turtle::sc;
+
+namespace plt = matplotlibcpp;
 
 int main() {
     turtle::sc::dbg_assert(true, "");
@@ -11,9 +15,28 @@ int main() {
     std::vector<Vector2f> ctrl_pts;
 
     ctrl_pts.push_back(Vector2f(-1.0, 0.0));
-    ctrl_pts.push_back(Vector2f(-0.5, -0.5));
-    ctrl_pts.push_back(Vector2f(0.5, 0.5));
+    ctrl_pts.push_back(Vector2f(-0.5, 0.5));
+    ctrl_pts.push_back(Vector2f(0.5, -0.5));
     ctrl_pts.push_back(Vector2f(1.0, 0.0));
 
     bezier_spline bs = bezier_spline::bezier_curve(ctrl_pts, 0.01);
+
+    std::vector<float> x(bs.num_pts());
+    std::vector<float> y(bs.num_pts());
+    for (int i = 0; i < bs.num_pts(); ++i) {
+        x[i] = bs.pts(i, 0);
+        y[i] = bs.pts(i, 1);
+    }
+
+    std::vector<float> x2(bs.num_pts());
+    std::vector<float> y2(bs.num_pts());
+    for (int i = 0; i < bs.ctrl_pts[0].size(); ++i) {
+        x2[i] = bs.ctrl_pts[0][i].x();
+        y2[i] = bs.ctrl_pts[0][i].y();
+    }
+
+
+    plt::plot(x, y, "");
+    plt::plot(x2, y2, "ro");
+    plt::show();
 }
